796d4be7ed7dbcabbff9a1e19d2db0a510a93e26
tya <tya15185891357@163.com>
2021-02-19 09:26:44 +0800

    测试

---
+++ Test_1117/src/com/tya/For99.java
@@ -0,0 +1,21 @@
+package com.tya;
+
+public class For99 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 9; i++) {
+
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print(j + "*" + i + "=" + i * j + '\t');
+
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+}
